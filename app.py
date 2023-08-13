c659aa7a6009fcc0f66a861b73dabd6b36836813
Sai <90199989+djsaikrishna@users.noreply.github.com>
2023-08-13 21:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+# Prompt the user to input the URL
+url = input("Enter the URL you want to scrape: ")
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        url = request.form['url']
-        output_file = "output.txt"
+# Get the filename for the output text file
+output_file = "output.txt"
 
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        links = soup.find_all("a")
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+links = soup.find_all("a")
 
-        with open(output_file, "w") as f:
-            for link in links:
-                href = link.get("href")
-                if href:
-                    f.write(href + "\n")
+with open(output_file, "w") as f:
+    for link in links:
+        href = link.get("href")
+        if href:
+            # Get the plain text content of the link and remove leading/trailing spaces
+            text_content = link.get_text(strip=True)
+            f.write(text_content + "\n")
 
-        return render_template('index.html', links=links, output_file=output_file)
-
-    return render_template('index.html', links=[])
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print("Text content saved to", output_file)

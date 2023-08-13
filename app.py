5c7b409a00dbd18e9f186c921f2c398825ac61f5
Sai <90199989+djsaikrishna@users.noreply.github.com>
2023-08-13 21:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/', methods=['GET', 'POST'])
 def scrape_links():
